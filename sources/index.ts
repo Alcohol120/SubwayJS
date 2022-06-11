20c5207532f990332220b2365efdc69a9a4c2d17
Alcohol120 <alco.alclab@gmail.com>
2022-06-11 06:54:37 +0300

    export amendment;

--- sources/index.ts
+++ sources/index.ts
@@ -1,2 +1,5 @@
 export { Map } from 'Subway/Map';
+export { Route } from 'Subway/Route';
+export { Request } from 'Subway/Request';
 export { Middleware } from 'Subway/Middleware';
+export type { TOnLoad } from 'Subway/Route';
